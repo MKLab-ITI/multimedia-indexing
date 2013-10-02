8a54a40b69b4261a4b8cf5bdf640146539981bfd
Eleftherios Spyromitros-Xioufis <l3fman@gmail.com>
2013-10-02 17:29:54 +0300

    Updates to the image download classes

--- src/main/java/gr/iti/mklab/download/ImageDownload.java
+++ src/main/java/gr/iti/mklab/download/ImageDownload.java
@@ -1,5 +1,8 @@
 package gr.iti.mklab.download;
 
+import gr.iti.mklab.visual.extraction.ImageScaling;
+import gr.iti.mklab.visual.utilities.ImageIOGreyScale;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,11 +16,9 @@ import java.util.concurrent.Callable;
 
 import javax.imageio.ImageIO;
 
-import gr.iti.mklab.visual.extraction.ImageScaling;
-import gr.iti.mklab.visual.utilities.ImageIOGreyScale;
-
 /**
- * Class for downloading images.
+ * This class implements the Callable interface and can be used for
+ * multi-threaded image download.
  * 
  * @author Eleftherios Spyromitros-Xioufis
  * 
@@ -29,50 +30,149 @@ public class ImageDownload implements Callable<ImageDownload> {
 	 */
 	private String urlStr;
 
+	/**
+	 * The image identifier.
+	 */
 	private String id;
 
-	private String downloadFolder = null;
+	/**
+	 * The directory where the image will be downloaded (temporarily or
+	 * permanently).
+	 */
+	private String downloadFolder;
 
+	/**
+	 * Whether to store a thumbnail of the downloaded image.
+	 */
+	private boolean saveThumb;
+
+	/**
+	 * Whether to store the original image.
+	 */
+	private boolean saveOriginal;
+
+	/**
+	 * Whether to follow redirects (note that when redirects are followed, a
+	 * wrong image may be downloaded).
+	 */
+	private boolean followRedirects;
+
+	/**
+	 * The BufferedImage object.
+	 */
 	private BufferedImage image;
 
-	private BufferedImage scaledImage;
-
+	/**
+	 * The value to use in HttpURLConnection.setConnectTimeout()
+	 */
 	public static final int connectionTimeout = 5000;
-	public static final int maxRetries = 2;
 
+	/**
+	 * The value to use in HttpURLConnection.setReadTimeout()
+	 */
+	public static final int readTimeout = 5000;
+
+	/**
+	 * The number of connection retries.
+	 */
+	public static final int maxRetries = 0; // currently not used
+
+	/**
+	 * The size of the thumbnail in pixels.
+	 */
+	public static final int thumbnailSizeInPixels = 200 * 200;
+
+	/**
+	 * If set to true, debug output is displayed.
+	 */
+	public boolean debug = false;
+
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+	}
+
+	/**
+	 * Constructor with 3 arguments.
+	 * 
+	 * @param urlStr
+	 *            The url where the image is downloaded from
+	 * @param id
+	 *            The image identifier
+	 * @param downloadFolder
+	 *            The folder where the image is temporarily downloaded
+	 */
 	public ImageDownload(String urlStr, String id, String downloadFolder) {
 		this.urlStr = urlStr;
 		this.id = id;
 		this.downloadFolder = downloadFolder;
+		this.saveThumb = false;
+		this.saveOriginal = false;
+		this.followRedirects = false;
+	}
+
+	/**
+	 * Constructor with 6 arguments.
+	 * 
+	 * @param urlStr
+	 *            The url where the image is downloaded from
+	 * @param id
+	 *            The image identifier (used to name the image file after
+	 *            download)
+	 * @param downloadFolder
+	 *            The folder where the image is downloaded
+	 * @param saveThumb
+	 *            Whether a thumbnail of the image should be saved
+	 * @param saveOriginal
+	 *            Whether the original image should be saved
+	 * @param followRedirects
+	 *            Whether redirects should be followed
+	 */
+	public ImageDownload(String urlStr, String id, String downloadFolder,
+			boolean saveThumb, boolean saveOriginal, boolean followRedirects) {
+		this.urlStr = urlStr;
+		this.id = id;
+		this.downloadFolder = downloadFolder;
+		this.saveThumb = saveThumb;
+		this.saveOriginal = saveOriginal;
+		this.followRedirects = followRedirects;
 	}
 
 	@Override
+	/**
+	 * Returns an ImageDownload object from where the BufferedImage object and the image identifier can be
+	 * obtained.
+	 */
 	public ImageDownload call() throws Exception {
-		// System.out.println("Downloading image " + urlStr);
+		if (debug)
+			System.out.println("Downloading image " + urlStr + " started.");
 		downloadImage();
-		// System.out.println("Downloading image " + urlStr + " took " + (end - start) + " ms");
+		if (debug)
+			System.out.println("Downloading image " + urlStr + " completed.");
 		return this;
 	}
 
 	/**
 	 * Download of an image by URL.
 	 * 
-	 * @param urlStr
-	 *            The URL of a Photo
 	 * @throws Exception
 	 */
 	private void downloadImage() throws Exception {
-		// try to recognize the type of the image from the url.
-		// in case that the url does not contain a known image extension a jpg extension.
+		// try to recognize the type of the image from the url so that the
+		// correct format and file extension
+		// are used when saving the thumbnail image or the original image.
+		// In case that the url does not contain a known image extension, the
+		// jpg extension is used.
 		String[] splitted = urlStr.split("\\.");
 		String fileExtension = (splitted[splitted.length - 1]).toLowerCase();
-		if (!fileExtension.equals("jpg") && !fileExtension.equals("jpeg") && !fileExtension.equals("png") && !fileExtension.equals("bmp") && !fileExtension.equals("gif")) {
+		if (!fileExtension.equals("jpg") && !fileExtension.equals("jpeg")
+				&& !fileExtension.equals("png") && !fileExtension.equals("bmp")
+				&& !fileExtension.equals("gif")) {
 			fileExtension = "jpg";
 		}
 		// this name filename will be used for the saved image file
 		String imageFilename = downloadFolder + id + "." + fileExtension;
 
-		// initiallize the url, checking that it is valid
+		// initialize the url, checking that it is valid
 		URL url = null;
 		try {
 			url = new URL(urlStr);
@@ -85,79 +185,142 @@ public class ImageDownload implements Callable<ImageDownload> {
 		FileOutputStream fos = null;
 		ReadableByteChannel rbc = null;
 		InputStream in = null;
+		boolean success = false;
 		try {
 			conn = (HttpURLConnection) url.openConnection();
-			// when redirects are followed, a wrong image may be downloaded, e.g. in Flickr
-			// HttpURLConnection.setFollowRedirects(true);
-			// System.out.println(conn.getConnectTimeout());
-			conn.setInstanceFollowRedirects(false);
-			conn.setConnectTimeout(connectionTimeout); // TO DO: add retries when connections times out
-			conn.setReadTimeout(connectionTimeout);
-			conn.connect(); // open connection
-			in = conn.getInputStream();
-			rbc = Channels.newChannel(in);
+			conn.setInstanceFollowRedirects(followRedirects);
+			conn.setConnectTimeout(connectionTimeout); // TO DO: add retries
+														// when connections
+														// times out
+			conn.setReadTimeout(readTimeout);
+			conn.connect();
+			success = true;
 		} catch (Exception e) {
-			System.out.println("Connection related exception at url: " + urlStr + " closing connection and streams..");
-			rbc.close();
-			in.close();
-			conn.disconnect(); // close the connection
+			System.out
+					.println("Connection related exception at url: " + urlStr);
 			throw e;
+		} finally {
+			if (!success) {
+				conn.disconnect();
+			}
 		}
 
-		// if an Exception is thrown in the following code, the file that was created must be deleted.
+		success = false;
 		try {
-			// first just copy the file
-			fos = new FileOutputStream(imageFilename);
-			fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-			fos.close();
-			rbc.close();
-			in.close();
-			conn.disconnect(); // close the connection
+			in = conn.getInputStream();
+			success = true;
+		} catch (Exception e) {
+			System.out
+					.println("Exception when getting the input stream from the connection at url: "
+							+ urlStr);
+			throw e;
+		} finally {
+			if (!success) {
+				in.close();
+			}
+		}
 
+		rbc = Channels.newChannel(in);
+
+		// if an Exception is thrown in the following code, the file that was
+		// created must be deleted
+		try {
+			// just copy the file
+			fos = new FileOutputStream(imageFilename);
+			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			fos.close();
+			in.close();
+			rbc.close();
+			conn.disconnect();
 			try { // first try reading with the default class
 				image = ImageIO.read(new File(imageFilename));
-			} catch (IllegalArgumentException e) { // if it fails retry with the corrected class
-				// This exception is probably because of a grayscale jpeg image.
-				System.out.println("Exception: " + e.getMessage() + " | Image: " + imageFilename + " URL: " + urlStr);
-				image = ImageIOGreyScale.read(new File(imageFilename));
+			} catch (IllegalArgumentException e) {
+				// this exception is probably thrown because of a greyscale jpeg
+				// image
+				System.out.println("Exception: " + e.getMessage()
+						+ " | Image: " + imageFilename + " URL: " + urlStr);
+				image = ImageIOGreyScale.read(new File(imageFilename)); // retry
+																		// with
+																		// the
+																		// modified
+																		// class
 			}
-		} catch (Exception e) { // in case of any other exception delete the image and re-throw the exception
-			System.out.println("Exception: " + e.getMessage() + " | Image: " + imageFilename + " URL: " + urlStr);
+		} catch (Exception e) { // in case of any other exception delete the
+								// image and re-throw the exception
+			System.out.println("Exception: " + e.toString() + " | Image: "
+					+ imageFilename + " URL: " + urlStr);
 			throw (e);
 		} finally {
-			if (image == null) {
+			if (image == null) { // if the image could not be read into a
+									// BufferedImage object then delete it
 				File imageFile = new File(imageFilename);
 				imageFile.delete();
-				System.out.println("Deleting image with " + id + " url: " + urlStr);
+				System.out.println("Deleting image with id " + id + ", url: "
+						+ urlStr);
 				throw new Exception("Could not read into BufferedImage");
+			} else {
+				if (saveThumb) { // save a thumbnail of the original image
+					ImageScaling scale = new ImageScaling(thumbnailSizeInPixels);
+					BufferedImage scaledImage = scale.maxPixelsScaling(image);
+					FileOutputStream out = new FileOutputStream(new File(
+							imageFilename.replace("." + fileExtension,
+									"-thumb." + fileExtension)));
+					ImageIO.write(scaledImage, fileExtension, out);
+					out.close();
+				}
+				if (!saveOriginal) {// delete the original image if needed
+					File imageFile = new File(imageFilename);
+					imageFile.delete();
+				}
 			}
-
-			if (image != null) {
-				// save a scaled instance of the original file
-				ImageScaling scale = new ImageScaling(200 * 200);
-				scaledImage = scale.maxPixelsScaling(image);
-				FileOutputStream out = new FileOutputStream(new File(imageFilename));
-				ImageIO.write(scaledImage, fileExtension, out);
-				out.close();
-			}
-
+			// close the open streams
 			fos.close();
 			in.close();
 			rbc.close();
-			conn.disconnect(); // close the connection
+			conn.disconnect();
 		}
-
 	}
 
 	public String getUrlStr() {
 		return urlStr;
 	}
 
-	public BufferedImage getIm() {
+	public BufferedImage getImage() {
 		return image;
 	}
 
 	public String getId() {
 		return id;
+	}
+
+	/**
+	 * Example of a single image download using this class.
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String args[]) {
+		String urlStr = "http://upload.wikimedia.org/wikipedia/commons/5/58/Sunset_2007-1.jpg";
+		String id = "Sunset_2007-1";
+		String downloadFolder = "images/";
+		boolean saveThumb = true;
+		boolean saveOriginal = true;
+		boolean followRedirects = false;
+		ImageDownload imdown = new ImageDownload(urlStr, id, downloadFolder,
+				saveThumb, saveOriginal, followRedirects);
+		imdown.setDebug(true);
+		try {
+			imdown.call();
+			System.out
+					.println("Getting the BufferedImage object of the downloaded image.");
+			imdown.getImage();
+			System.out
+					.println("Reading the downloaded image thumbnail into a BufferedImage object.");
+			ImageIO.read(new File(downloadFolder + id + "-thumb.jpg"));
+			System.out.println("Success!");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/gr/iti/mklab/download/ImageDownloader.java
+++ src/main/java/gr/iti/mklab/download/ImageDownloader.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,7 +12,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Class for multi-threaded image download.
+ * This class implements multi-threaded image downloading.
  * 
  * @author Eleftherios Spyromitros-Xioufis
  * 
@@ -26,10 +25,75 @@ public class ImageDownloader {
 
 	private int numPendingTasks;
 
+	/**
+	 * Used to limit the memory usage.
+	 */
+	private int maxNumPendingTasks;
+
+	private boolean saveOriginal;
+
+	private boolean saveThumb;
+
+	private boolean followRedirects;
+
 	public ImageDownloader(int numThreads) {
 		downloadExecutor = Executors.newFixedThreadPool(numThreads);
 		pool = new ExecutorCompletionService<ImageDownload>(downloadExecutor);
 		numPendingTasks = 0;
+		maxNumPendingTasks = 20;
+		saveOriginal = false;
+		saveThumb = true;
+		followRedirects = false;
+	}
+
+	public ImageDownloader(int numThreads, int maxNumPendingTasks) {
+		this(numThreads);
+		this.maxNumPendingTasks = maxNumPendingTasks;
+	}
+
+	/**
+	 * Gets an image download results from the pool.
+	 * 
+	 * @return the download result, or null in no results are ready
+	 * @throws Exception
+	 *             for a failed download task
+	 */
+	public ImageDownload getImageDownloadResult() throws Exception {
+		Future<ImageDownload> future = pool.poll();
+		if (future == null) { // no completed tasks in the pool
+			return null;
+		} else {
+			try {
+				ImageDownload imd = future.get();
+				return imd;
+			} catch (Exception e) {
+				throw e;
+			} finally {
+				// in any case (Exception or not) the numPendingTask should be
+				// reduced
+				numPendingTasks--;
+			}
+		}
+	}
+
+	/**
+	 * Gets an image download results from the pool, waiting if necessary.
+	 * 
+	 * @return the download result
+	 * @throws Exception
+	 *             for a failed download task
+	 */
+	public ImageDownload getImageDownloadResultWait() throws Exception {
+		try {
+			ImageDownload imd = pool.take().get();
+			return imd;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			// in any case (Exception or not) the numPendingTask should be
+			// reduced
+			numPendingTasks--;
+		}
 	}
 
 	/**
@@ -41,99 +105,89 @@ public class ImageDownloader {
 		return numPendingTasks;
 	}
 
+	/**
+	 * Returns true if the number of pending tasks is smaller than the maximum
+	 * allowable number.
+	 * 
+	 * @return
+	 */
+	public boolean canAcceptMoreTasks() {
+		if (numPendingTasks < maxNumPendingTasks) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void setFollowRedirects(boolean followRedirects) {
+		this.followRedirects = followRedirects;
+	}
+
+	public void setSaveOriginal(boolean saveOriginal) {
+		this.saveOriginal = saveOriginal;
+	}
+
+	public void setSaveThumb(boolean saveThumb) {
+		this.saveThumb = saveThumb;
+	}
+
+	/**
+	 * Shut the download executor down, waiting for up to 10 seconds for the
+	 * remaining tasks to complete.
+	 * 
+	 * @throws InterruptedException
+	 */
 	public void shutDown() throws InterruptedException {
 		downloadExecutor.shutdown();
 		downloadExecutor.awaitTermination(10, TimeUnit.SECONDS);
 	}
 
 	/**
+	 * Submits a new image download task.
 	 * 
 	 * @param URL
-	 *            The url of the image file.
+	 *            The url of the image
 	 * @param id
-	 *            An id used to name the image file after downloading.
+	 *            The id of the image (used to name the image file after
+	 *            download)
 	 * @param downloadFolder
-	 *            The folder where the image file is downloaded.
+	 *            The folder where the image file is downloaded
 	 */
-	public void submitImageDownloadTask(String URL, String id, String downloadFolder) {
-		Callable<ImageDownload> call = new ImageDownload(URL, id, downloadFolder);
+	public void submitImageDownloadTask(String URL, String id,
+			String downloadFolder) {
+		Callable<ImageDownload> call = new ImageDownload(URL, id,
+				downloadFolder, saveThumb, saveOriginal, followRedirects);
 		pool.submit(call);
 		numPendingTasks++;
-		// System.out.println("Pending download tasks: " + numPendingTasks);
 	}
 
 	/**
-	 * Gets an image download results from the pool.
+	 * This method exemplifies multi-threaded image download from a list of
+	 * urls. It uses 5 download threads.
 	 * 
-	 * @return the download result, or null in no results are ready
-	 * @throws ExecutionException
-	 * @throws InterruptedException
-	 */
-	public ImageDownload getImageDownloadResult() throws Exception {
-		Future<ImageDownload> future = pool.poll();
-		if (future == null) {
-			return null;
-		}// no completed tasks in the pull
-		else {
-			try {
-				ImageDownload imd = future.get();
-				numPendingTasks--;
-				return imd;
-			} catch (Exception e) {
-				numPendingTasks--;
-				throw e;
-			} // in any case (Exception or not) the numPendingTask should be reduced
-		}
-	}
-
-	/**
-	 * Gets an image download results from the pool, waiting if necessary.
-	 * 
-	 * @return the download result
-	 * @throws ExecutionException
-	 * @throws InterruptedException
-	 */
-	public ImageDownload getImageDownloadResultWait() throws Exception {
-		try {
-			ImageDownload imd = pool.take().get();
-			numPendingTasks--;
-			return imd;
-		} catch (Exception e) {
-			numPendingTasks--;
-			throw e;
-		} // in any case (Exception or not) the numPendingTask should be reduced
-	}
-
-	/**
-	 * @param args
+	 * @param dowloadFolder
+	 *            Full path to the folder where the images are downloaded
+	 * @param urlsFile
+	 *            Full path to the file that contains the ids and urls (space
+	 *            separated) of the images (one per line)
+	 * @param numUrls
+	 *            The total number of urls to consider
+	 * @param urlsToSkip
+	 *            How many urls (from the top of the file to be skipped)
 	 * @throws Exception
 	 */
-	public static void main(String[] args) throws Exception {
-		// String dowloadFolder = args[0];
-		// String urlsFile = args[1];
-		// int numUrls = Integer.parseInt(args[2]);
-
-		String dowloadFolder = "C:/Users/lef/Desktop/ITI/data/ImageNET/images/";
-		String urlsFile = "C:/Users/lef/Desktop/ITI/data/ImageNET/urls_sample2.txt";
-		int numUrls = 100;
-		int urlsToSkip = 0;
-
-		downloadFromUrlsFile(dowloadFolder, urlsFile, numUrls, urlsToSkip);
-	}
-
-	public static void downloadFromUrlsFile(String dowloadFolder, String urlsFile, int numUrls, int urlsToSkip) throws Exception {
-		// Example of multi-threaded image download and write to a folder.
+	public static void downloadFromUrlsFile(String dowloadFolder,
+			String urlsFile, int numUrls, int urlsToSkip) throws Exception {
 		long start = System.currentTimeMillis();
-
-		int numThreads = 5;
-		int maxNumPendingTasks = 10;
-		BufferedReader in = new BufferedReader(new FileReader(new File(urlsFile)));
-
+		int numThreads = 20;
+		int maxNumPendingTasks = numThreads * 10;
+		BufferedReader in = new BufferedReader(new FileReader(
+				new File(urlsFile)));
 		for (int i = 0; i < urlsToSkip; i++) {
 			in.readLine();
 		}
-
-		ImageDownloader downloader = new ImageDownloader(numThreads);
+		ImageDownloader downloader = new ImageDownloader(numThreads,
+				maxNumPendingTasks);
 		int submittedCounter = 0;
 		int completedCounter = 0;
 		int failedCounter = 0;
@@ -141,46 +195,53 @@ public class ImageDownloader {
 		while (true) {
 			String url;
 			String id = "";
-			// if we can submit more tasks to the downloader then do it
-			while (submittedCounter < numUrls) {
-				if (downloader.getNumPendingTasks() == maxNumPendingTasks) {
-					break;
-				}
-				System.out.println("IF1");
+			// if there are more task to submit and the downloader can accept
+			// more tasks then submit
+			while (submittedCounter < numUrls
+					&& downloader.canAcceptMoreTasks()) {
 				line = in.readLine();
 				url = line.split("\\s+")[1];
 				id = line.split("\\s+")[0];
 				downloader.submitImageDownloadTask(url, id, dowloadFolder);
 				submittedCounter++;
 			}
-			// if the completed and failed tasks are less than the submitted task,
-			// try to consume a submitted task
+			// if are submitted taks that are pending completion ,try to consume
 			if (completedCounter + failedCounter < submittedCounter) {
-				System.out.println("IF2");
 				try {
 					downloader.getImageDownloadResultWait();
 					completedCounter++;
-					System.out.println(completedCounter + " downloads completed!");
-				} catch (NullPointerException e) {
-					// do nothing, means that there is no download ready for consumption
+					System.out.println(completedCounter
+							+ " downloads completed!");
 				} catch (Exception e) {
-					System.out.println(e.getMessage());
 					failedCounter++;
 					System.out.println(failedCounter + " downloads failed!");
+					System.out.println(e.getMessage());
 				}
 			}
 			// if all tasks have been consumed then break;
 			if (completedCounter + failedCounter == numUrls) {
-				System.out.println("IF3");
 				downloader.shutDown();
 				in.close();
 				break;
 			}
 		}
-
 		long end = System.currentTimeMillis();
 		System.out.println("Total time: " + (end - start) + " ms");
 		System.out.println("Downloaded images: " + completedCounter);
 		System.out.println("Failed images: " + failedCounter);
+	}
+
+	/**
+	 * Calls the downloadFromUrlsFile.
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		String dowloadFolder = "images/";
+		String urlsFile = "urls.txt";
+		int numUrls = 1000;
+		int urlsToSkip = 0;
+		downloadFromUrlsFile(dowloadFolder, urlsFile, numUrls, urlsToSkip);
 	}
 }
